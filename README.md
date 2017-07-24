3b1ca642cabb07399482dd455aa566df6344f078
Lucas Borges <jlucasb19@gmail.com>
2017-07-24 17:59:49 -0300

    Cria serviço de retorno de tweets baseado em uma string de busca

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ Busca em sites de social media
 pip install flask, twitter-application-only-auth, requests
 
 no terminal execute:
-$ export FLASK_APP=run.py
+$ export FLASK_APP=back/run.py
+$ export FLASK_DEBUG=1
 
 para rodar o servidor execute:
 $ flask run
--- back/run.py
+++ back/run.py
@@ -3,25 +3,29 @@
 # $ export FLASK_APP=run.py
 # para rodar o servidor execute:
 # $ flask run
-from flask import Flask, render_template
+from flask import Flask
 from application_only_auth import Client
 import json
 import requests
 from urllib.parse import quote
 app = Flask(__name__)
 
-@app.route("/twitter")
-def hello():
+@app.route("/twitter/<search_string>")
+def hello(search_string):
     CONSUMER_KEY = 'sbmaTK8blnFbLo4FeDxe6HkDm'
     CONSUMER_SECRET = 'aRgoP3t1BOs322MXDbyaVABH5Vfqjy44bKwur2r8UPkb6Ij1pH'
 
     client = Client(CONSUMER_KEY, CONSUMER_SECRET)
-    tweets = client.request('https://api.twitter.com/1.1/search/tweets.json?q=%23python&result_type=recent')
-    tweet_text = ""
+    tweets = client.request("https://api.twitter.com/1.1/search/tweets.json?q={0}&result_type=recent".format(search_string))
+
+    response = {}
+    i = 0
     for tweet in tweets['statuses']:
         url = "https://twitter.com/{0}/status/{1}".format(tweet['user']['screen_name'], tweet['id_str'])
         url = quote(url, safe='')
-        r = requests.get('https://publish.twitter.com/oembed?url=' + url)
-        txt = json.loads(r.text)
-        tweet_text += txt['html']
-    return tweet_text
+        response[i] = url
+        # r = requests.get('https://publish.twitter.com/oembed?url=' + url)
+        # txt = json.loads(r.text)
+        # tweet_text += txt['html']
+        i = i + 1
+    return json.dumps(response)
