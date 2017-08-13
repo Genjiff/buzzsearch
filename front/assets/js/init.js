041db1a17e9ec0e08a88e7cfc841cda526f60f4e
Victor Martinez <vcrmartinez@gmail.com>
2017-08-13 12:23:41 -0300

    Show user search

--- front/assets/js/init.js
+++ front/assets/js/init.js
@@ -1,20 +1,21 @@
 var techName = getTechName();
+document.querySelector('#search-key-section h2').innerHTML += "<b>" + techName + "</b>";
 
 // Called by maps api as a callback
 function initMap() {
-	var uluru = {lat: -25.363, lng: 131.044};
-	var map = new google.maps.Map(document.getElementById('meetupMapResults'), {
-	  zoom: 1,
-	  center: uluru
-	});
+    var uluru = {lat: -25.363, lng: 131.044};
+    var map = new google.maps.Map(document.getElementById('meetupMapResults'), {
+        zoom: 1,
+        center: uluru
+    });
 
-	var meetupUrl = getMeetupUrl(techName);
-	renderMeetupResults(map, meetupUrl);
+    var meetupUrl = getMeetupUrl(techName);
+    renderMeetupResults(map, meetupUrl);
 }
 
 function initTweets() {
-	var twitterUrl = getTwitterUrl(techName);
-	renderTwitterResults(twitterUrl, 'tweets-section');
+    var twitterUrl = getTwitterUrl(techName);
+    renderTwitterResults(twitterUrl, 'tweets-section');
 }
 
 initTweets();
