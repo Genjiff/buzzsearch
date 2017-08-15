cf9b1ef0b77d43aa1019973f2bfad1a5c3462bae
Victor Martinez <vcrmartinez@gmail.com>
2017-08-15 19:35:04 -0300

    Show content box in gmaps

--- front/assets/js/requests.js
+++ front/assets/js/requests.js
@@ -84,6 +84,33 @@ function renderGithubResults(url, section) {
     xhr.send();
 }
 
+function getFormattedLocation(techEvent) {
+    console.log(techEvent);
+    if (techEvent.venue == undefined) return "";
+
+    var content = "<p>Local: ";
+    if (techEvent.venue.city !== undefined) {
+        content += techEvent.venue.city;
+    }
+
+    if (techEvent.venue.state !== undefined) {
+        content += " - " + techEvent.venue.state;
+    }
+
+    if (techEvent.venue.country !== undefined) {
+        content += " - " + techEvent.venue.country.toUpperCase();
+    }    
+
+    content += "</p><br />"
+    return content;
+}
+
+function getInfoWindow(techEvent) {
+    var location = getFormattedLocation(techEvent);
+    return "<h1>"+techEvent.name+"</h1><br /><br />"
+           + location + 
+           "<a href='" + techEvent.link + "' target='_blank'>Acessar página</a>";
+}
 
 function renderMeetupResults(map, url) {
     console.log("Requesting", url);
@@ -95,11 +122,22 @@ function renderMeetupResults(map, url) {
         if (xhr.status === 200) {
             var json = JSON.parse(xhr.responseText);
             json.forEach(function (techEvent) {
+                var content = getInfoWindow(techEvent);
+                var infowindow = new google.maps.InfoWindow({
+                    content: content
+                });
+
                 var position = {'lat': techEvent.group.lat, 'lng': techEvent.group.lon}
                 var marker = new google.maps.Marker({
                     position: position,
-                    map: map
+                    map: map,
+                    title: techEvent.name
                 });
+                marker.addListener('click', function() {
+                    infowindow.open(map, marker);
+                });
+
+                
             });
 
             var loading = document.querySelector('#maps-section .loading');
