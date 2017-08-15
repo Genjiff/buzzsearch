38a302fb144ac0aa221e95ccbe0c336d11c05ccd
Victor Martinez <vcrmartinez@gmail.com>
2017-08-15 19:38:04 -0300

    Remove unnecessary comment

--- front/assets/js/requests.js
+++ front/assets/js/requests.js
@@ -114,7 +114,6 @@ function getInfoWindow(techEvent) {
 
 function renderMeetupResults(map, url) {
     console.log("Requesting", url);
-    // see https://developers.google.com/maps/documentation/javascript/infowindows#open
     var xhr = new XMLHttpRequest();
     xhr.open("GET", url, true);
 
